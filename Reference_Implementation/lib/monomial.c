58dccbe58e2d50166c2e727915f2ab2efd2140c1
Kamyar Mohajerani <kammoh@gmail.com>
2024-12-19 21:23:54 -0500

    fixed build and merge issues

--- Reference_Implementation/lib/monomial.c
+++ Reference_Implementation/lib/monomial.c
@@ -145,16 +145,16 @@ void monomial_mat_seed_expand_rnd(monomial_t *res,
     yt_shuffle_state(&shake_monomial_state, res->permutation);
 
 }
-
-/* samples a random perm matrix */
-void monomial_mat_rnd(monomial_t *res) {
-   fq_star_rnd_elements(res->coefficients, N);
-   for(uint32_t i = 0; i < N; i++) {
-      res->permutation[i] = i;
-   }
-   /* FY shuffle on the permutation */
-   yt_shuffle(res->permutation);
-} /* end monomial_mat_rnd */
+//
+// /* samples a random perm matrix */
+// void monomial_mat_rnd(monomial_t *res) {
+//    fq_star_rnd_elements(res->coefficients, N);
+//    for(uint32_t i = 0; i < N; i++) {
+//       res->permutation[i] = i;
+//    }
+//    /* FY shuffle on the permutation */
+//    yt_shuffle(res->permutation);
+// } /* end monomial_mat_rnd */
 
 /// \param res[out] = to_invert**-1
 /// \param to_invert[in]
